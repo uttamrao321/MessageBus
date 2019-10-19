1a237bf72f3d8dab633c55493f3b707fc0223262
Uttam Rao <56537200+uttamrao321@users.noreply.github.com>
2019-10-19 16:50:08 -0400

    Add files via upload

--- SubClient/SubClient.cpp
+++ SubClient/SubClient.cpp
@@ -1,6 +1,8 @@
 #include "pch.h"
+#include<Windows.h>
 #include <iostream>
 #include <ctime>
+#include <vector>
 #include "StdApp.h"
 #include "SubClient.h"
 
@@ -16,17 +18,30 @@ unique_ptr<StdApp> StdApp::instance()
 
 void SubClient::setup()
 {
-	setClientType("sub");
-	//LOGDEBUG("Sample Publisher - Setup");
+    //set object type as subscriber
+	setClientType("sub"); 
+
+    //Initial load from cache
+    log(LOGDEBUG, "Initial load from cache");
+    vector<string> cache = getCache();
+    for (vector<string>::iterator it = cache.begin(); it != cache.end(); it++)
+    {
+        string msg = *it;
+        log(LOGDEBUG, msg);
+    }
+
 }//void SubClient::setup()
 
 void SubClient::onIdle()
 {
-	//Quick & Dirty Subscriber
+    // continues subscription to messages
 	string msg = fetch();
 	if (msg.length() > 0)
 	{
-		cout << "Your Message - " << msg.c_str() << std::endl;
+        log(LOGDEBUG, msg);
 	}
+  
+    //shutdown(true);
+    //or leave it running like a service
 }//void SubClient::onIdle()
 
--- SubClient/SubClient.h
+++ SubClient/SubClient.h
@@ -3,11 +3,12 @@
 
 using namespace StdAppLib;
 
+/** A simple subscriber to cache svr and message bus
+ */
 class SubClient :public StdApp
 {
 public:
 	SubClient() { name = __func__; }
 	void setup()  override;
 	void onIdle() override;
-
 };
